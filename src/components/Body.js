90fc7472eb01d802d102aa7e0d047026317e4590
Shweta Sarsar <shwetasarsar@gmail.com>
2025-08-26 18:27:16 +0530

    fix: added route

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,35 +3,47 @@ import { useState, useEffect } from "react";
 
 const Body =() =>{
     const [listOfRestaurants, setListOfRestaurants] = useState([]);
-
-    const filterBtnHandler = () =>{
-        const filteredList = listOfRestaurants.filter((res)=>res?.card?.card?.info?.avgRating > 4.6);
-        setListOfRestaurants(filteredList);
-    }
-
-    const clearBtnHandler = () =>{
-        fetchData();
-    }
+    const [filteredRestaurants, setfilteredRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState("");
 
     useEffect(()=>{
         fetchData();
     }, [])
 
+    const filterBtnHandler = () =>{
+        const filteredList = listOfRestaurants.filter((res)=>res?.card?.card?.info?.avgRating === 5);
+        setfilteredRestaurants(filteredList);
+    }
+
+    const clearBtnHandler = () =>{
+        fetchData();
+        setSearchText("");
+    }
+
     const fetchData = async () =>{
         const data = await fetch("https://mocki.io/v1/f0241638-27f4-454a-ba4d-0a8efea2de55")
         const json = await data.json();
+        console.log(json);
         setListOfRestaurants(json.data);
+        setfilteredRestaurants(json.data);
+    }
+
+    const searchHandler = () =>{
+        const filteredRes = listOfRestaurants.filter((res) => res?.card?.card?.info?.name.toLowerCase().includes(searchText.toLowerCase()))
+        setfilteredRestaurants(filteredRes);
     }
 
     return (
         <div className="body-container">
             <div className="btn-container">
-                <button onClick={filterBtnHandler} style={{cursor: "pointer"}}>Filter Button</button>
-                <button onClick={clearBtnHandler} style={{cursor: "pointer"}}>Clear Filter</button>
+                <input type="text" onChange={(e)=>{setSearchText(e.target.value)}} value={searchText}/>
+                <button onClick={searchHandler}>Search</button>
+                <button onClick={filterBtnHandler} style={{cursor: "pointer"}}>Top rated restaurant</button>
+                <button onClick={clearBtnHandler} style={{cursor: "pointer"}}>Clear</button>
             </div>
             <div className="res-container">
             {
-                listOfRestaurants.map((restaurant)=> <RestaurantCard resData={restaurant} key={restaurant.card.card.info.id}/>)
+                filteredRestaurants.map((restaurant)=> <RestaurantCard resData={restaurant} key={restaurant.card.card.info.id}/>)
             }
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () =>{
+    const [btnName, setBtnName] = useState("Login");
+
+    const btnHandler =()=>{
+        btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+    }
+
     return (
         <div className="header">
             <div className="logo-container">
-                <img src={LOGO_URL} alt="logo" className="logo"/>
+                <Link to="/"><img src={LOGO_URL} alt="logo" className="logo"/></Link>
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Us</Link></li>
+                    <li><Link to="/contact">Contact Us</Link></li>
                     <li>Cart</li>
+                    <button className="login-btn" onClick={btnHandler}>{btnName}</button>
                 </ul>
             </div>
         </div>
