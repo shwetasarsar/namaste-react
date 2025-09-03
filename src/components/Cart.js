a978c61f15c7f982e9f1e4f1e0237e2422ab48e1
Shweta Sarsar <shwetasarsar@gmail.com>
2025-09-03 17:29:27 +0530

    added remaining

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,14 +6,15 @@ import { clearItem } from '../utils/cartSlice';
 const Cart = () => {
 
   const itemList = useSelector((store)=> store.cart.items);
-  console.log("itemlist", itemList)
+  // console.log("itemlist", itemList)
 
   const dispatch = useDispatch();
 
   return (
     <div className='text-center w-6/12 mx-auto'>
-      <div className='font-bold text-2xl'>Cart</div>
-      <button onClick={() => dispatch(clearItem())} className='bg-black text-white p-2 rounded-lg cursor-pointer'>Clear Cart</button>
+      <div className='font-bold text-2xl p-4'>Cart</div>
+      <button onClick={() => dispatch(clearItem())} className='bg-black text-white px-4 py-2 rounded-lg cursor-pointer'>Clear Cart</button>
+      <div className='m-4'>{!itemList.length && "Cart is empty, Add items to the cart!"}</div>
       <>
       {
         itemList.map((item,index)=>(
